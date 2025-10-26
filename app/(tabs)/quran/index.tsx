6d2c7eb5d1e90489a7ec26e125cccc2526515095
Abu Safiia <abusafiia@Mac.fritz.box>
2025-10-26 16:54:53 +0100

    remove interceptor

--- app/(tabs)/quran/index.tsx
+++ app/(tabs)/quran/index.tsx
@@ -12,18 +12,25 @@ export default function Quran() {
   //test api
   const fetchQuran = async () => {
     try {
-      const response = await axiosInstance.get("/resources/chapter_reciters");
-      console.log(response.data);
+      setLoading(true);
+      const response = await axiosInstance.get("/reciters?language=eng");
+
       setReciters(
-        response.data.reciters.map((reciter: Reciter) => ({
-          ...reciter,
-          photo_url: "",
-          arabic_name: "إبراهيم الأخضر",
-        }))
+        response.data.reciters
+          .sort((a: Reciter, b: Reciter) => {
+            if (a.letter < b.letter) return -1;
+            if (a.letter > b.letter) return 1;
+            return 0;
+          })
+          .map((reciter: Reciter) => ({
+            ...reciter,
+            photo_url: "",
+          }))
       );
+      setLoading(false);
     } catch (error) {
-      setError(error as string);
-      console.error(error, "error hier");
+      setError(error instanceof Error ? error.message : String(error));
+      console.error(error);
     } finally {
       setLoading(false);
     }
@@ -32,16 +39,44 @@ export default function Quran() {
     fetchQuran();
   }, []);
 
+  if (error) {
+    return (
+      <SafeAreaView className="flex-1 items-center justify-center bg-qasid-black">
+        <Text className="text-qasid-gold text-lg mb-2">{error}</Text>
+      </SafeAreaView>
+    );
+  }
+
+  if (loading) {
+    return (
+      <SafeAreaView className="flex-1 bg-qasid-black">
+        <View className="flex-1 justify-center items-center">
+          <Text className="text-qasid-gold text-lg mb-2">
+            Loading reciters...
+          </Text>
+          <View
+            style={{
+              width: 40,
+              height: 40,
+              borderWidth: 4,
+              borderColor: "#E7C11C44",
+              borderTopColor: "#E7C11C",
+              borderRadius: 20,
+              marginTop: 8,
+            }}
+            className="animate-spin"
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView className="flex-1 bg-qasid-black">
       <ScrollView>
         <View className="flex-1 gap-3">
           {reciters.map((reciter) => (
-            <ReciterCard
-              key={reciter.id}
-              reciter={reciter}
-              onPress={(event) => console.log(event)}
-            />
+            <ReciterCard key={reciter.id} reciter={reciter} href={reciter.id} />
           ))}
         </View>
       </ScrollView>
